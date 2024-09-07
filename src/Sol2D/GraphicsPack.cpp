0f2eb52a3f5763d0e599fba013a5773a575592c6
brainstream <brainstream@yandex.ru>
2024-09-07 18:43:28 +0400

    The GraphicsPack added to replace SpriteAnimation

---
+++ src/Sol2D/GraphicsPack.cpp
@@ -0,0 +1,287 @@
+// Sol2D Game Engine
+// Copyright (C) 2023-2024 Sergey Smolyannikov aka brainstream
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Lesser Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include <Sol2D/GraphicsPack.h>
+#include <Sol2D/Utils/Math.h>
+
+using namespace Sol2D;
+using namespace Sol2D::Utils;
+
+GraphicsPack::GraphicsPack(SDL_Renderer & _renderer) :
+    mp_renderer(&_renderer),
+    m_current_frame_index(0),
+    m_current_frame_duration(std::chrono::milliseconds::zero()),
+    m_total_duration(std::chrono::milliseconds::zero())
+{
+}
+
+GraphicsPack::~GraphicsPack()
+{
+    for(Frame * frame : m_frames)
+        delete frame;
+}
+
+size_t GraphicsPack::addFrame(const GraphicsPackFrameOptions & _options)
+{
+    Frame * frame = new Frame(_options);
+    m_frames.push_back(frame);
+    if(frame->is_visible)
+        m_total_duration += _options.duration;
+    return m_frames.size() - 1;
+}
+
+size_t GraphicsPack::insertFrame(size_t _index, const GraphicsPackFrameOptions & _options)
+{
+    Frame * frame = new Frame(_options);
+    m_frames.insert(m_frames.begin() + _index, frame);
+    if(frame->is_visible)
+        m_total_duration += _options.duration;
+    return _index;
+}
+
+bool GraphicsPack::removeFrame(size_t _index)
+{
+    auto it = m_frames.begin() + _index;
+    if(it == m_frames.end())
+        return false;
+    Frame * frame = *it;
+    if(frame->is_visible)
+        m_total_duration -= frame->duration;
+    m_frames.erase(it);
+    if(m_current_frame_index >= m_frames.size())
+    {
+        m_current_frame_index = 0;
+        m_current_frame_duration = std::chrono::milliseconds::zero();
+    }
+    return true;
+}
+
+bool GraphicsPack::setFrameVisibility(size_t _index, bool _is_visible)
+{
+    if(_index < m_frames.size())
+    {
+        Frame * frame = m_frames[_index];
+        if(frame->is_visible && !_is_visible)
+        {
+            m_total_duration -= frame->duration;
+            frame->is_visible = false;
+        }
+        else if(!frame->is_visible && _is_visible)
+        {
+            m_total_duration += frame->duration;
+            frame->is_visible = true;
+        }
+        return true;
+    }
+    return false;
+}
+
+std::optional<bool> GraphicsPack::isFrameVisible(size_t _index) const
+{
+    if(_index < m_frames.size())
+    {
+        return m_frames[_index]->is_visible;
+    }
+    return std::optional<bool>();
+}
+
+bool GraphicsPack::setFrameDuration(size_t _index, std::chrono::milliseconds & _duration)
+{
+    if(_index < m_frames.size())
+    {
+        Frame * frame = m_frames[_index];
+        if(frame->is_visible)
+            m_total_duration = m_total_duration - frame->duration + _duration;
+        frame->duration = _duration;
+        return true;
+    }
+    return false;
+}
+
+std::optional<std::chrono::milliseconds> GraphicsPack::getFrameDuration(size_t _index) const
+{
+    if(_index < m_frames.size())
+    {
+        return m_frames[_index]->duration;
+    }
+    return std::optional<std::chrono::milliseconds>();
+}
+
+std::pair<bool, size_t> GraphicsPack::addSprite(
+    size_t _frame,
+    const Sprite & _sprite,
+    const GraphicsPackSpriteOptions & _options)
+{
+    if(_frame >= m_frames.size())
+        return std::make_pair(false, 0);
+    std::vector<Graphics> & graphics = m_frames[_frame]->graphics;
+    graphics.push_back(Graphics(_sprite, _options));
+    return std::make_pair(true, graphics.size() - 1);
+}
+
+std::pair<bool, size_t> GraphicsPack::addSprite(
+    size_t _frame,
+    const SpriteSheet & _sprite_sheet,
+    size_t _sprite_index,
+    const GraphicsPackSpriteOptions & _options)
+{
+    if(_frame >= m_frames.size() || !_sprite_sheet.isValid())
+        return std::make_pair(false, 0);
+    const std::vector<Rect> & sheet_rects = _sprite_sheet.getRects();
+    if(_sprite_index >= sheet_rects.size())
+        return std::make_pair(false, 0);
+    std::vector<Graphics> & graphics = m_frames[_frame]->graphics;
+    graphics.push_back(Graphics(
+        _sprite_sheet.getTexture(),
+        sheet_rects[_sprite_index],
+        sheet_rects[_sprite_index].getSize(),
+        _options
+    ));
+    return std::make_pair(true, graphics.size() - 1);
+}
+
+std::pair<bool, size_t> GraphicsPack::addSprites(
+    size_t _frame,
+    const SpriteSheet & _sprite_sheet,
+    std::vector<size_t> _sprite_indices,
+    const GraphicsPackSpriteOptions & _options)
+{
+    if(_frame >= m_frames.size() || !_sprite_sheet.isValid())
+        return std::make_pair(false, 0);
+    const std::vector<Rect> & sheet_rects = _sprite_sheet.getRects();
+    for(size_t idx : _sprite_indices)
+    {
+        if(idx >= sheet_rects.size())
+            return std::make_pair(false, 0);
+    }
+    std::vector<Graphics> & graphics = m_frames[_frame]->graphics;
+    graphics.reserve(graphics.size() + _sprite_indices.size());
+    for(size_t i = 0; i < _sprite_indices.size(); ++i)
+    {
+        graphics.push_back(Graphics(
+            _sprite_sheet.getTexture(),
+            sheet_rects[_sprite_indices[i]],
+            sheet_rects[_sprite_indices[i]].getSize(),
+            _options
+        ));
+    }
+    return std::make_pair(true, graphics.size() - 1);
+}
+
+bool GraphicsPack::removeSprite(size_t _frame, size_t _sprite)
+{
+    if(_frame >= m_frames.size())
+        return false;
+    std::vector<Graphics> & graphics = m_frames[_frame]->graphics;
+    if(_sprite >= graphics.size())
+        return false;
+    graphics.erase(graphics.begin() + _sprite);
+    return true;
+}
+
+void GraphicsPack::render(const Point & _point, std::chrono::milliseconds _time_passed)
+{
+    if(m_total_duration == std::chrono::milliseconds::zero())
+    {
+        performRender(_point);
+    }
+    m_current_frame_duration += _time_passed;
+    Frame * frame = m_frames[m_current_frame_index];
+    if(!frame->is_visible)
+    {
+        frame = switchToNextVisibleFrame();
+        if(frame)
+        {
+            m_current_frame_duration = _time_passed;
+        }
+        else
+        {
+            m_current_frame_duration = std::chrono::milliseconds::zero();
+            return;
+        }
+    }
+    for(;;)
+    {
+        if(frame->duration < m_current_frame_duration)
+        {
+            m_current_frame_duration -= frame->duration;
+            frame = switchToNextVisibleFrame();
+        }
+        else
+        {
+            break;
+        }
+    }
+    performRender(_point);
+}
+
+GraphicsPack::Frame * GraphicsPack::switchToNextVisibleFrame()
+{
+    for(size_t i = m_current_frame_index + 1; i < m_frames.size(); ++i)
+    {
+        if(m_frames[i]->is_visible)
+        {
+            m_current_frame_index = i;
+            return m_frames[i];
+        }
+    }
+    if(m_current_frame_index > 0)
+    {
+        for(size_t i = 0; i < m_current_frame_index; ++i)
+        {
+            if(m_frames[i]->is_visible)
+            {
+                m_current_frame_index = i;
+                return m_frames[i];
+            }
+        }
+    }
+    return m_frames[m_current_frame_index]->is_visible ? m_frames[m_current_frame_index] : nullptr;
+}
+
+void GraphicsPack::performRender(const Point & _point)
+{
+    if(m_frames.empty())
+        return;
+    Frame * frame = m_frames[m_current_frame_index];
+    if(!frame->is_visible)
+        return;
+    for(const Graphics & graphics : frame->graphics)
+    {
+        SDL_FRect dest_rect
+        {
+            .x = _point.x - graphics.position.x,
+            .y = _point.y - graphics.position.y,
+            .w = graphics.dest_size.w,
+            .h = graphics.dest_size.h
+        };
+        union { int as_int; SDL_FlipMode as_flip_mode; } flip;
+        flip.as_int = SDL_FLIP_NONE;
+        if(graphics.is_flipped_horizontally)
+            flip.as_int |= SDL_FLIP_HORIZONTAL;
+        if(graphics.is_flipped_vertically)
+            flip.as_int |= SDL_FLIP_VERTICAL;
+        SDL_RenderTextureRotated(
+            mp_renderer,
+            graphics.texture.get(),
+            graphics.src_rect.toSdlPtr(),
+            &dest_rect,
+            radiansToDegrees(graphics.angle_rad),
+            graphics.flip_center.has_value() ? graphics.flip_center->toSdlPtr() : nullptr,
+            flip.as_flip_mode
+        );
+    }
+}
---
+++ src/Sol2D/GraphicsPack.h
@@ -0,0 +1,144 @@
+// Sol2D Game Engine
+// Copyright (C) 2023-2024 Sergey Smolyannikov aka brainstream
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Lesser Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <Sol2D/SpriteSheet.h>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+#include <optional>
+#include <chrono>
+#include <vector>
+
+namespace Sol2D {
+
+struct GraphicsPackFrameOptions
+{
+    std::optional<std::chrono::milliseconds> duration;
+    std::optional<bool> is_visible;
+};
+
+struct GraphicsPackSpriteOptions
+{
+    std::optional<Point> position;
+    std::optional<float> angle_rad;
+    std::optional<std::optional<Point>> flip_center;
+    std::optional<bool> is_flipped_horizontally;
+    std::optional<bool> is_flipped_vertically;
+    std::optional<bool> is_visible;
+};
+
+class GraphicsPack final
+{
+    struct Graphics
+    {
+        S2_DEFAULT_COPY_AND_MOVE(Graphics)
+
+        Graphics(const Sprite & _sprite, const GraphicsPackSpriteOptions & _options) :
+            Graphics(_sprite.getTexture(), _sprite.getSourceRect(), _sprite.getDestinationSize(), _options)
+        {
+        }
+
+        Graphics(
+            std::shared_ptr<SDL_Texture> _texture,
+            const Rect & _src_rect,
+            const Size & _dest_size,
+            const GraphicsPackSpriteOptions & _options
+        ) :
+            texture(_texture),
+            src_rect(_src_rect),
+            dest_size(_dest_size),
+            position(_options.position.value_or(Point {.0f, .0f})),
+            angle_rad(_options.angle_rad.value_or(.0f)),
+            flip_center(_options.flip_center.value_or(Point { .0f, .0f })),
+            is_flipped_horizontally(_options.is_flipped_horizontally.value_or(false)),
+            is_flipped_vertically(_options.is_flipped_vertically.value_or(false)),
+            is_visible(_options.is_visible.value_or(true))
+        {
+        }
+
+        std::shared_ptr<SDL_Texture> texture;
+        Rect src_rect;
+        Size dest_size;
+        Point position;
+        float angle_rad;
+        std::optional<Point> flip_center;
+        bool is_flipped_horizontally;
+        bool is_flipped_vertically;
+        bool is_visible;
+        std::shared_ptr<Sprite> sprite;
+    };
+
+    struct Frame
+    {
+        S2_DEFAULT_COPY_AND_MOVE(Frame)
+
+        Frame(const GraphicsPackFrameOptions & _options) :
+            duration(_options.duration.value_or(std::chrono::milliseconds::zero())),
+            is_visible(_options.is_visible.value_or(true))
+        {
+        }
+
+        std::chrono::milliseconds duration;
+        bool is_visible;
+        std::vector<Graphics> graphics;
+    };
+
+public:
+    S2_DEFAULT_COPY_AND_MOVE(GraphicsPack)
+
+    explicit GraphicsPack(SDL_Renderer & _renderer);
+    ~GraphicsPack();
+
+    size_t addFrame(const GraphicsPackFrameOptions & _options);
+    size_t insertFrame(size_t _index, const GraphicsPackFrameOptions & _options);
+    bool removeFrame(size_t _index);
+    bool setFrameVisibility(size_t _index, bool _is_visible);
+    std::optional<bool> isFrameVisible(size_t _index) const;
+    bool setFrameDuration(size_t _index, std::chrono::milliseconds & _duration);
+    std::optional<std::chrono::milliseconds> getFrameDuration(size_t _index) const;
+    std::pair<bool, size_t> addSprite(
+        size_t _frame,
+        const Sprite & _sprite,
+        const GraphicsPackSpriteOptions & _options);
+    std::pair<bool, size_t> addSprite(
+        size_t _frame,
+        const SpriteSheet & _sprite_sheet,
+        size_t _sprite_index,
+        const GraphicsPackSpriteOptions & _options);
+    std::pair<bool, size_t> addSprites(
+        size_t _frame,
+        const SpriteSheet & _sprite_sheet,
+        std::vector<size_t> _sprite_indices,
+        const GraphicsPackSpriteOptions & _options);
+    bool removeSprite(size_t _frame, size_t _sprite);
+    void render(const Point & _point, std::chrono::milliseconds _time_passed);
+
+private:
+    void performRender(const Point & _point);
+    Frame * switchToNextVisibleFrame();
+
+private:
+    SDL_Renderer * mp_renderer;
+    std::vector<Frame *> m_frames;
+    size_t m_current_frame_index;
+    std::chrono::milliseconds m_current_frame_duration;
+    std::chrono::milliseconds m_total_duration;
+};
+
+} // namespace Sol2D
